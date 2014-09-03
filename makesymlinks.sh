38bac4195b945e76c110f98bd2668a4e2e2b119b
Greatman Lim <greatman.lim@mahti.org>
2014-09-03 06:59:31 +0300

    Preparing files to university computers.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 ############################
+# .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="xmodmap-mac2pc xmodmap-swapper inputrc vimperatorrc vimrc Xdefaults zshrc"   # list of files/folders to symlink in homedir
+dir=~/dotfiles # dotfiles directory
+olddir=~/dotfiles_old # old dotfiles backup directory
+files="bashrc vimrc xmodmap-mac2pc zshrc inputrc vimperatorrc Xdefaults xmodmap-swapper" # list of files/folders to symlink in homedir
 
 ##########
 
@@ -29,36 +30,30 @@ for file in $files; do
     ln -s $dir/$file ~/.$file
 done
 
-<<COMMENT
-
-# zsh is installed seperately
-
 install_zsh () {
-# Test to see if zshell is installed.  If it is:
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-    if [[ ! -d $dir/oh-my-zsh/ ]]; then
-        git clone http://github.com/michaeljsmalley/oh-my-zsh.git
+    # Test to see if zshell is installed. If it is:
+    if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+        # Clone my oh-my-zsh repository from GitHub only if it isn't already present
+        if [[ ! -d $dir/oh-my-zsh/ ]]; then
+            git clone http://github.com/robbyrussell/oh-my-zsh.git
+        fi
+        # Set the default shell to zsh if it isn't currently set to zsh
+        if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+            chsh -s $(which zsh)
+        fi
+    else
+        # If zsh isn't installed, get the platform of the current machine
+        platform=$(uname);
+        # If the platform is Linux, try an apt-get to install zsh and then recurse
+        if [[ $platform == 'Linux' ]]; then
+            sudo apt-get install zsh
+            install_zsh
+            # If the platform is OS X, tell the user to install zsh :)
+        elif [[ $platform == 'Darwin' ]]; then
+            echo "Please install zsh, then re-run this script!"
+            exit
+        fi
     fi
-    # Set the default shell to zsh if it isn't currently set to zsh
-    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-        chsh -s $(which zsh)
-    fi
-else
-    # If zsh isn't installed, get the platform of the current machine
-    platform=$(uname);
-    # If the platform is Linux, try an apt-get to install zsh and then recurse
-    if [[ $platform == 'Linux' ]]; then
-        sudo apt-get install zsh
-        install_zsh
-    # If the platform is OS X, tell the user to install zsh :)
-    elif [[ $platform == 'Darwin' ]]; then
-        echo "Please install zsh, then re-run this script!"
-        exit
-    fi
-fi
 }
 
-COMMENT
-
-# install_zsh
+install_zsh
--- zshrc
+++ zshrc
@@ -1,11 +1,12 @@
 # Path to your oh-my-zsh configuration.
-ZSH=$HOME/.oh-my-zsh
+export ZSH=$HOME/.oh-my-zsh
 
 # Set name of the theme to load.
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
-ZSH_THEME="jonathan"
+# ZSH_THEME="agnoster"
+ZSH_THEME="af-magic"
 
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
