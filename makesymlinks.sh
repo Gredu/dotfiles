c23f2f5b69a6b394097575e30d3390df00c0cfb8
Greatman Lim <greatman.lim@mahti.org>
2015-01-07 20:22:22 +0200

    Add rtorrentrc.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles # dotfiles directory
 olddir=~/dotfiles_old # old dotfiles backup directory
-files="bashrc vimrc xmodmap-mac2pc zshrc inputrc vimperatorrc xinitrc xmodmap-swapper" # list of files/folders to symlink in homedir
+files="bashrc vimrc xmodmap-mac2pc zshrc inputrc vimperatorrc xinitrc xmodmap-swapper rtorrent.rc" # list of files/folders to symlink in homedir
 
 ##########
 
