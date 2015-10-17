df48b5f5735e4438d4ed8f5c3a72f32fc284f02e
Greatman Lim <greatman.lim@mahti.org>
2015-10-17 18:39:54 +0300

    Typing mistake.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles # dotfiles directory
 olddir=~/dotfiles_old # old dotfiles backup directory
-files="vimrc zshrc inputrc vimperatorrc xinitrc rtorrent.rc vifrc" # list of files/folders to symlink in homedir
+files="vimrc zshrc inputrc vimperatorrc xinitrc rtorrent.rc vifmrc" # list of files/folders to symlink in homedir
 
 ##########
 
