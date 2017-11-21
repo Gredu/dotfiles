fd50acf507a013fce8dc06fe18f008d519a070ab
Greatman Lim <greatman.lim@mahti.org>
2017-11-21 21:27:00 +0200

    Fix, string should be boolean

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -609,7 +609,7 @@
 ##   - true
 ##   - false
 ##   - ask
-c.content.notifications = 'true'
+c.content.notifications = True
 
 ## Enable pdf.js to view PDF files in the browser. Note that the files
 ## can still be downloaded by clicking the download button in the pdf.js
