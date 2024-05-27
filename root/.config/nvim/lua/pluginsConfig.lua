ce421e7b3c41d441f69eb114e519bc1f106ed613
Greatman Lim <lim.greatman@gmail.com>
2024-05-27 15:55:18 +0300

    Sort lspconfig

--- root/.config/nvim/lua/pluginsConfig.lua
+++ root/.config/nvim/lua/pluginsConfig.lua
@@ -172,14 +172,14 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protoc
 require("mason").setup()
 require("mason-lspconfig").setup()
 
-require('lspconfig').tsserver.setup{cababilities=cababilities}
-require('lspconfig').pyright.setup{cababilities=cababilities}
-require('lspconfig').denols.setup{cababilities=cababilities}
-require('lspconfig').vuels.setup{cababilities=cababilities}
 require('lspconfig').bashls.setup{cababilities=cababilities}
+require('lspconfig').denols.setup{cababilities=cababilities}
 require('lspconfig').intelephense.setup{cababilities=cababilities}
-require('lspconfig').svelte.setup{cababilities=cababilities}
 require('lspconfig').jdtls.setup{cababilities=cababilities}
+require('lspconfig').pyright.setup{cababilities=cababilities}
+require('lspconfig').svelte.setup{cababilities=cababilities}
+require('lspconfig').tsserver.setup{cababilities=cababilities}
+require('lspconfig').vuels.setup{cababilities=cababilities}
 
 -- Circles
 require('circles').setup({
