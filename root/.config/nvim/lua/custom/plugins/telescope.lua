845fd2ad182aefcc4c82eeb328e28d332b9ca22d
Greatman Lim <greatman.lim@mahti.org>
2024-06-25 17:30:53 +0300

    Add symbols

--- root/.config/nvim/lua/custom/plugins/telescope.lua
+++ root/.config/nvim/lua/custom/plugins/telescope.lua
@@ -1,8 +1,12 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
-    config = function() 
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'nvim-telescope/telescope-symbols.nvim',
+    },
+    config = function()
       require("custom.telescope")
     end
   }
