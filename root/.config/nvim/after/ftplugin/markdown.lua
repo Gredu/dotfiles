f49560d4ce01e96ac614149ea5520981f93a24c3
Greatman Lim <lim.greatman@gmail.com>
2024-09-24 14:48:19 +0300

    Use wrap and linebreaks in markdown

--- root/.config/nvim/after/ftplugin/markdown.lua
+++ root/.config/nvim/after/ftplugin/markdown.lua
@@ -1,5 +1,6 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap=true, silent=true }
-vim.opt.wrap = 'lbr'
+vim.opt.wrap = true
+vim.opt.linebreak = true
 map('n', 'j', 'gj', opts)
 map('n', 'k', 'gk', opts)
