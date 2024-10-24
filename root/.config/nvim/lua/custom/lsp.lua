a0d65688b87eb6a06778ae7b15330ef86c5b71fc
Greatman Lim <greatman.lim@mahti.org>
2024-10-24 16:47:26 +0300

    Update deprecated server tsserver

--- root/.config/nvim/lua/custom/lsp.lua
+++ root/.config/nvim/lua/custom/lsp.lua
@@ -23,9 +23,9 @@ require('lspconfig').volar.setup({
 })
 local mason_packages = vim.fn.stdpath("data") .. "/mason/packages"
 local volar_path = mason_packages .. "/vue-language-server/node_modules/@vue/language-server"
-require("lspconfig").tsserver.setup({
+require("lspconfig").ts_ls.setup({
   -- NOTE: To enable Hybrid Mode, change hybrideMode to true above and uncomment the following filetypes block.
-  -- WARN: THIS MAY CAUSE HIGHLIGHTING ISSUES WITHIN THE TEMPLATE SCOPE WHEN TSSERVER ATTACHES TO VUE FILES
+  -- WARN: THIS MAY CAUSE HIGHLIGHTING ISSUES WITHIN THE TEMPLATE SCOPE WHEN ts_ls ATTACHES TO VUE FILES
 
   -- filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
   init_options = {
