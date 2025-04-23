1779a4216cecb78dc0bf36f8d217f2aa573cda9c
Greatman Lim <lim.greatman@gmail.com>
2025-04-23 12:16:01 +0300

    Fix hilight from ibl not working

--- root/.config/nvim/lua/custom/ibl.lua
+++ root/.config/nvim/lua/custom/ibl.lua
@@ -1,9 +1,13 @@
+vim.cmd([[ highlight IblScope guifg=#545c7e gui=nocombine ]])
+
 require('ibl').setup {
   indent = {
     char = '¦',
   },
   scope = {
     enabled = true,
-    show_start = false,
+    show_start = true,
+    show_end = true,
+    highlight = { "IblScope" },
   },
 }
