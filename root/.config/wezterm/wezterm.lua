b61bb2318935307cd1db5a66b2eb4403bf3f43a7
gredu <greatman.lim@mahti.org>
2025-05-18 13:29:52 +0300

    Config tweaks for tabs

--- root/.config/wezterm/wezterm.lua
+++ root/.config/wezterm/wezterm.lua
@@ -16,6 +16,8 @@ config.font = wezterm.font_with_fallback {
 config.window_decorations = 'RESIZE'
 config.use_fancy_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_max_width = 50
 
 config.max_fps = 120
 
