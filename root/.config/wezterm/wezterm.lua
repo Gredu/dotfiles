38b15e705b927492166941a0798d4f42d0751330
gredu <greatman.lim@mahti.org>
2025-05-21 09:43:01 +0300

    Hide tab bat when there are only one tab

--- root/.config/wezterm/wezterm.lua
+++ root/.config/wezterm/wezterm.lua
@@ -16,7 +16,7 @@ config.font = wezterm.font_with_fallback {
 config.window_decorations = 'RESIZE'
 config.use_fancy_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
-config.hide_tab_bar_if_only_one_tab = false
+config.hide_tab_bar_if_only_one_tab = true
 config.tab_max_width = 50
 
 config.max_fps = 120
