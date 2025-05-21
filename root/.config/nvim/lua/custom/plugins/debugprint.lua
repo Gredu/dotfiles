1d4b064b83b60cf6caf4715512fe5f10568829b5
gredu <greatman.lim@mahti.org>
2025-05-21 10:48:15 +0300

    Start debugprint plugin

--- root/.config/nvim/lua/custom/plugins/debugprint.lua
+++ root/.config/nvim/lua/custom/plugins/debugprint.lua
@@ -4,6 +4,11 @@ return {
     "echasnovski/mini.nvim",         -- Optional: Needed for line highlighting (full mini.nvim plugin)
     "nvim-telescope/telescope.nvim", -- Optional: If you want to use the :SearchDebugPrints command with telescope.nvim
   },
+  config = function()
+    require('debugprint').setup {
+      -- config
+    }
+  end,
   lazy = false, -- Required to make line highlighting work before debugprint is first used
   version = "*", -- Remove if you DON'T want to use the stable version
 }
