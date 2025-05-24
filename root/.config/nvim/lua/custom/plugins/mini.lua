ec82cb423ac5ee2d37ad26d24c9e3b6a9ebfc982
gredu <greatman.lim@mahti.org>
2025-05-24 23:56:48 +0300

    Remove notify plugin from mini

--- root/.config/nvim/lua/custom/plugins/mini.lua
+++ root/.config/nvim/lua/custom/plugins/mini.lua
@@ -4,7 +4,6 @@ return {
     config = function()
       require("mini.ai").setup()
       require("mini.align").setup()
-      require("mini.notify").setup()
       require("mini.pairs").setup()
       require("mini.surround").setup()
     end,
