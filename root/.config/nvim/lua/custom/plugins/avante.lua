8098bc30ae476df4249bee13f18a3618fb75845d
gredu <greatman.lim@mahti.org>
2025-06-26 16:51:25 +0300

    Update Avante configuration

--- root/.config/nvim/lua/custom/plugins/avante.lua
+++ root/.config/nvim/lua/custom/plugins/avante.lua
@@ -1,45 +1,47 @@
 return {
   "yetone/avante.nvim",
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  -- ⚠️ must add this setting! ! !
+  build = function()
+    -- conditionally use the correct build system for the current OS
+    if vim.fn.has("win32") == 1 then
+      return "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+    else
+      return "make"
+    end
+  end,
   event = "VeryLazy",
   version = false, -- Never set this value to "*"! Never!
+  ---@module 'avante'
+  ---@type avante.Config
   opts = {
+    -- add any opts here
+    -- for example
+    provider = "claude",
     providers = {
       claude = {
         endpoint = "https://api.anthropic.com",
-        model = "claude-3-5-sonnet-20241022",
-        extra_request_body = {
-          temperature = 0,
-          max_tokens = 4096,
-          reasoning_effort = 'medium',
-        },
+        model = "claude-sonnet-4-20250514",
+        timeout = 30000, -- Timeout in milliseconds
+          extra_request_body = {
+            temperature = 0.75,
+            max_tokens = 20480,
+          },
       },
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o",
-        extra_request_body = {
-          timeout = 30000,
-          temperature = 0,
-          max_completion_tokens = 8192,
-          reasoning_effort = "medium",
-        },
-      }
     },
   },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
     -- "echasnovski/mini.pick", -- for file_selector provider mini.pick
     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-    "ibhagwan/fzf-lua", -- for file_selector provider fzf
+    -- "ibhagwan/fzf-lua", -- for file_selector provider fzf
+    -- "stevearc/dressing.nvim", -- for input provider dressing
+    -- "folke/snacks.nvim", -- for input provider snacks
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    -- "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
