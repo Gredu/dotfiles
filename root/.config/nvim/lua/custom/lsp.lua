ffffedf60289816ac5ae13e75759a7ecf1e76172
Greatman Lim <lim.greatman@gmail.com>
2025-10-10 18:16:45 +0300

    Update nvim lua configs

--- root/.config/nvim/lua/custom/lsp.lua
+++ root/.config/nvim/lua/custom/lsp.lua
@@ -1,39 +1,7 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
-require('lspconfig').volar.setup({
-  -- NOTE: Uncomment to enable volar in file types other than vue.
-  -- (Similar to Takeover Mode)
 
-  -- filetypes = { "vue", "javascript", "typescript", "javascriptreact", "typescriptreact", "json" },
-  init_options = {
-    vue = {
-      hybridMode = false,
-    },
-    -- NOTE: This might not be needed. Uncomment if you encounter issues.
-
-    -- typescript = {
-    --   tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
-    -- },
-  },
-})
-local mason_packages = vim.fn.stdpath("data") .. "/mason/packages"
-local volar_path = mason_packages .. "/vue-language-server/node_modules/@vue/language-server"
-require("lspconfig").ts_ls.setup({
-  -- NOTE: To enable Hybrid Mode, change hybrideMode to true above and uncomment the following filetypes block.
-  -- WARN: THIS MAY CAUSE HIGHLIGHTING ISSUES WITHIN THE TEMPLATE SCOPE WHEN ts_ls ATTACHES TO VUE FILES
-
-  -- filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-  init_options = {
-    plugins = {
-      {
-        name = "@vue/typescript-plugin",
-        location = volar_path,
-        languages = { "vue" },
-      },
-    },
-  },
-})
-require("lspconfig").lua_ls.setup({
+vim.lsp.config('vue_ls', {
   settings = {
     Lua = {
       diagnostics = {
@@ -42,3 +10,5 @@ require("lspconfig").lua_ls.setup({
     }
   }
 })
+
+vim.lsp.enable('vue_ls')
