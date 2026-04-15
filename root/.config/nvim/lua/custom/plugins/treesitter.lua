bdf5ca8fa9cae9644c6e2658ecad77a74f526dd4
Greatman Lim <greatman.lim@mahti.org>
2026-04-15 17:57:55 +0300

    Update treesitter config for new API

--- root/.config/nvim/lua/custom/plugins/treesitter.lua
+++ root/.config/nvim/lua/custom/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    -- branch = "main",
+    branch = "main",
     lazy = false,
     config = function()
       require("custom.treesitter")
--- root/.config/nvim/lua/custom/treesitter.lua
+++ root/.config/nvim/lua/custom/treesitter.lua
@@ -1,22 +1,20 @@
-require("nvim-treesitter.configs").setup {
-  ensure_installed = {"javascript" , "lua", "java", "php", "html", "css", "scss", "vim", "make", "bash", "python", "typescript", "svelte", "vue"},
-  sync_install = true,
-  ignore_install = {},
-  highlight = {
-    enable = true,
-    disable= {},
-    additional_vim_regex_higlighting = false,
-  },
-  indent = {
-    enable = true,
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "gnn",
-      node_incremental = "grn",
-      node_decremental = "grm",
-      scope_incremental = "grc",
-    },
+require("nvim-treesitter").setup {
+  ensure_installed = {
+    "javascript",
+    "lua",
+    "java",
+    "php",
+    "html",
+    "css",
+    "scss",
+    "vim",
+    "make",
+    "bash",
+    "python",
+    "typescript",
+    "svelte",
+    "vue",
+    "markdown",
+    "markdown_inline",
   },
 }
