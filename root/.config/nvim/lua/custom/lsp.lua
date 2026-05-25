1bf7f28d029ec4866b4153afa27ae7dca0c55fb5
Greatman Lim <lim.greatman@gmail.com>
2026-05-25 18:11:51 +0300

    Fix vtsls not enabled

--- root/.config/nvim/lua/custom/lsp.lua
+++ root/.config/nvim/lua/custom/lsp.lua
@@ -1,6 +1,26 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
 
+local vue_language_server_path = vim.fn.expand('$MASON/packages/vue-language-server/node_modules/@vue/language-server')
+
+vim.lsp.config('vtsls', {
+  settings = {
+    vtsls = {
+      tsserver = {
+        globalPlugins = {
+          {
+            name = '@vue/typescript-plugin',
+            location = vue_language_server_path,
+            languages = { 'vue' },
+            configNamespace = 'typescript',
+          },
+        },
+      },
+    },
+  },
+  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+})
+
 vim.lsp.config('vue_ls', {
   settings = {
     Lua = {
@@ -11,4 +31,4 @@ vim.lsp.config('vue_ls', {
   }
 })
 
-vim.lsp.enable('vue_ls')
+vim.lsp.enable({ 'vtsls', 'vue_ls' })
