d09b1d293745615fb432fe6015e3cd5d43d2cb71
Greatman Lim <greatman.lim@mahti.org>
2026-08-27 17:19:32 +0300

    Improve network monitor

--- root/.xmonad/xmobar.hs
+++ root/.xmonad/xmobar.hs
@@ -4,12 +4,10 @@ Config {
     fgColor = "#c5c8c6",
     position = Top,
     commands = [
-        -- Run MultiCpu ["-t","cpu: <total0> <total1> <total2> <total3> <total4> <total5> <total6> <total7>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
-        -- Run MultiCpu ["-t","cpu: <usedratio>%","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
         Run MultiCpu ["-t","cpu: <total>%","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
-        Run Memory ["-t","mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run Swap ["-t","swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run DynNetwork ["-t","net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Memory ["-t","mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
+        Run Swap ["-t","swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
+        Run DynNetwork ["-t","<rx> \8595 <tx> \8593","-S","True","-d","1","-H","1048576","-L","65536","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-m","9","-c"," "] 10,
         Run Date "%a %b %_d %H:%M" "date" 10,
         Run DiskU [("/", "/ <used>/<size>"), ("/home", "~ <usedp>%")]
             ["-L", "20", "-H", "50", "-m", "1", "-p", "3", "-h", "#FFB6B0", "-l", "#CEFFAC", "-n", "#FFFFCC"]
